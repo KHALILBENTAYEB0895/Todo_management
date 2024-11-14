7e5d528cffbe2f9d3c4569205d281ffe3acbe84a
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-11-14 01:07:13 +0100

    Modify SpringSecurityConfig

--- src/main/java/ben/code/todo/config/SpringSecurityConfig.java
+++ src/main/java/ben/code/todo/config/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package ben.code.todo.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,8 +20,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
 
+    private UserDetailsService userDetailsService;
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -36,6 +41,11 @@ public class SpringSecurityConfig {
                     authorise.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
